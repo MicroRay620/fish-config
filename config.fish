72e8d86a8181d27499c66efd2ae23a83897a8e0a
MicroRay620 <microray620@gmail.com>
2025-09-12 21:48:02 -0400

    created

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+end
+
+if test -d ~/.linuxbrew/bin
+    set -gx PATH ~/.linuxbrew/bin $PATH
+else if test -d /home/linuxbrew/.linuxbrew/bin
+    set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+end
