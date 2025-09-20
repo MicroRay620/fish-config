872e6bd97793dfd2fdd090a70dcc0ebb4801d2d5
MicroRay620 <microray620@gmail.com>
2025-09-20 18:21:05 -0400

    started background process for the background changing

--- config.fish
+++ config.fish
@@ -13,10 +13,10 @@ set -gx VISUAL "nvim"
 set -U fish_user_paths /home/ruby/.local/bin $fish_user_paths
 set -x HYFETCH_DONT_WARN_RUST 1
 
+set -l time $(printf "%(%x | %X)T")
 
-#hyfetch
 # use this for random pride flag
 set -l RANDOM (date +%s)
-set -a flags femme transgender lesbian sapphic plural
+set -a flags femme transgender transfeminine lesbian sapphic finsexual
 set -l use_flag $flags[(math $RANDOM % (count $flags) + 1)]
 hyfetch -b fastfetch -p "$use_flag"
