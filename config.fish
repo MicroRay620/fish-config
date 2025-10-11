27137b8b564a1232f0f9423c557029f6666d5705
MicroRay620 <microray620@gmail.com>
2025-10-11 18:06:04 -0400

    added functions and made the organization better

--- config.fish
+++ config.fish
@@ -1,7 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here 
-end
-
 if test -d ~/.linuxbrew/bin
     set -gx PATH ~/.linuxbrew/bin $PATH
 else if test -d /home/linuxbrew/.linuxbrew/bin
@@ -16,19 +12,29 @@ set -x HYFETCH_DONT_WARN_RUST 1
 # set -l time $(printf "%T(%x | %X)")
 
 # use this for random pride flag
-set -l RANDOM (date +%s)
-set -a flags femme transgender transfeminine lesbian sapphic finsexual femboy gendernonconforming2 plural
-# Femme flag is a custom flag that I made
-# Femboy was added for Imp (an alter of mine)
-set -l use_flag $flags[(math $RANDOM % (count $flags) + 1)]
-hyfetch -b fastfetch -p "$use_flag"
+function RandomFlags
+    set -l RANDOM (date +%s)
+    set -a flags femme transgender transfeminine lesbian sapphic finsexual femboy gendernonconforming2 plural
+    # Femme flag is a custom flag that I made
+    # Femboy was added for Imp (an alter of mine)
+    set -l use_flag $flags[(math $RANDOM % (count $flags) + 1)]
+    hyfetch -b fastfetch -p "$use_flag"
+end
 
 # Alias
-alias neobim=nvim
-alias dir=ls 
-alias goto=cd
-# alias nf=neofetch # I do not have neofetch installed
-alias ff=fastfetch
-alias hf=hyfetch 
-alias aagl="flatpak run moe.launcher.an-anime-game-launcher"
-alias restart=reboot
+function Aliases
+    alias neobim=nvim
+    alias dir=ls 
+    alias goto=cd
+    # alias nf=neofetch # I do not have neofetch installed
+    alias ff=fastfetch
+    alias hf=hyfetch 
+    alias aagl="flatpak run moe.launcher.an-anime-game-launcher"
+    alias restart=reboot
+    alias fishconf="cd ~/.config/fish & nvim config.fish"
+    alias nvimconf="cd ~/.config/nvim & nvim"
+end
+
+# Function Calls
+RandomFlags
+Aliases
